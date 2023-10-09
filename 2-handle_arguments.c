b04e96431c2872ae9691bb1597ef3b182b3e52f3
Eng-Prud <prudence.n826@gmail.com>
2023-10-10 02:09:05 +0300

    Applied modifications to files: 2-handle_arguments.c shell.h

---
+++ 2-handle_arguments.c
@@ -0,0 +1,34 @@
+#include "simple.h"
+
+/**
+ * handle_command - Function that handles command line arguments
+ * @buffer: char to a ptr
+ */
+
+void handle_command(char *buffer)
+{
+	char *token;
+	char *args[100];
+	int arg_count = 0;
+
+	token = strtok(buffer, " ");
+	while (token != NULL)
+	{
+		args[arg_count] = token;
+		arg_count++;
+		token = strtok(NULL, " ");
+	}
+
+	if (fork() == 0)
+	{
+		if (execve(args[0], args, NULL) == -1)
+		{
+			perror(args[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		wait(NULL);
+	}
+}
--- shell.h
+++ shell.h
@@ -1,3 +1,12 @@
+#ifndef SHELL_H
+#define SHELL_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+
+void handle_command(char *buffer);
+
+#endif
