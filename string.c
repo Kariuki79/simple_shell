fbff13378cd533852078543ab5127a4fbf0e5ccf
Kariuki79 <kariukimwangi75@gmail.com>
2023-10-16 09:40:06 -0700

    string.c

--- string.c
+++ string.c
@@ -1,5 +1,4 @@
 #include "custom_shell.h"
-
 /**
  * custom_strlen - returns the length of a string
  * @s: The string whose length to check
@@ -8,15 +7,15 @@
  */
 int custom_strlen(char *s)
 {
-    int length = 0;
+	int length = 0;
 
-    if (!s)
-        return 0;
+	if (!s)
+		return (0);
 
-    while (*s++)
-        length++;
+	while (*s++)
+		length++;
 
-    return length;
+	return (length);
 }
 
 /**
@@ -28,19 +27,18 @@ int custom_strlen(char *s)
  */
 int custom_strcmp(char *s1, char *s2)
 {
-    while (*s1 && *s2)
-    {
-        if (*s1 != *s2)
-            return (*s1 - *s2);
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
 
-        s1++;
-        s2++;
-    }
-
-    if (*s1 == *s2)
-        return 0;
-    else
-        return (*s1 < *s2 ? -1 : 1);
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 < *s2 ? -1 : 1);
 }
 
 /**
@@ -52,13 +50,12 @@ int custom_strcmp(char *s1, char *s2)
  */
 char *custom_starts_with(const char *haystack, const char *needle)
 {
-    while (*needle)
-    {
-        if (*needle++ != *haystack++)
-            return NULL;
-    }
-    
-    return (char *)haystack;
+	while (*needle)
+	{
+		if (*needle++ != *haystack++)
+			return (NULL);
+	}
+	return ((char *)haystack);
 }
 
 /**
@@ -70,16 +67,15 @@ char *custom_starts_with(const char *haystack, const char *needle)
  */
 char *custom_strcat(char *dest, char *src)
 {
-    char *result = dest;
+	char *result = dest;
 
-    while (*dest)
-        dest++;
-    
-    while (*src)
-        *dest++ = *src++;
-    
-    *dest = *src;
+	while (*dest)
+		dest++;
 
-    return result;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+
+	return (result);
 }
 
