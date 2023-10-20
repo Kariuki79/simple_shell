2964194ec23265bfa0c446796f12b8fcb42e7b13
Prudence Njoroge <prudence.n826@gmail.com>
2023-10-20 15:30:34 -0700

    Applied modifications to files: lib.c str2.c strings.c

---
+++ lib.c
@@ -0,0 +1,95 @@
+#include "shell.h"
+
+/**
+ * _memcpy - copies info
+ * @newptr: destination ptr
+ * @ptr: source ptr
+ * @size: size of the new ptr
+ *
+ * Return: no return.
+ */
+void _memcpy(void *newptr, const void *ptr, unsigned int size)
+{
+	char *char_ptr = (char *)ptr;
+	char *char_newptr = (char *)newptr;
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		char_newptr[i] = char_ptr[i];
+}
+
+/**
+ * aux_itoa - Converts int to string.
+ * @n: type int no
+ * Return: String
+ */
+char *aux_itoa(int n)
+{
+	unsigned int n1;
+	int lenght = get_len(n);
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * (lenght + 1));
+	if (buffer == 0)
+		return (NULL);
+
+	*(buffer + lenght) = '\0';
+
+	if (n < 0)
+	{
+		n1 = n * -1;
+		buffer[0] = '-';
+	}
+	else
+	{
+		n1 = n;
+	}
+
+	lenght--;
+	do {
+		*(buffer + lenght) = (n1 % 10) + '0';
+		n1 = n1 / 10;
+		lenght--;
+	}
+	while (n1 > 0)
+		;
+	return (buffer);
+}
+
+/**
+ * get_sigint - Handle the crtl + c call in prompt
+ * @sig: Signal handler
+ */
+void get_sigint(int sig)
+{
+	(void)sig;
+	write(STDOUT_FILENO, "\n^-^ ", 5);
+}
+
+/**
+ * get_len - Get the lenght of a number.
+ * @n: type int number.
+ * Return: Lenght of a number.
+ */
+int get_len(int n)
+{
+	unsigned int n1;
+	int lenght = 1;
+
+	if (n < 0)
+	{
+		lenght++;
+		n1 = n * -1;
+	}
+	else
+	{
+		n1 = n;
+	}
+	while (n1 > 9)
+	{
+		lenght++;
+		n1 = n1 / 10;
+	}
+
+	return (lenght);
+}
---
+++ str2.c
@@ -0,0 +1,34 @@
+#include "shell.h"
+
+/**
+ * _strlen - Returns the lenght of a string.
+ * @s: Type char pointer
+ * Return: Always 0.
+ */
+int _strlen(const char *s)
+{
+	int len;
+
+	for (len = 0; s[len] != 0; len++)
+	{
+	}
+	return (len);
+}
+
+/**
+ * _strdup - duplicates a str in the heap memory.
+ * @s: Type char pointer str
+ * Return: duplicated str
+ */
+char *_strdup(const char *s)
+{
+	char *new;
+	size_t len;
+
+	len = _strlen(s);
+	new = malloc(sizeof(char) * (len + 1));
+	if (new == NULL)
+		return (NULL);
+	_memcpy(new, s, len + 1);
+	return (new);
+}
