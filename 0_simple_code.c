c4e918e8d170248e86a6b5c03691e9add1aafee8
Prudence Njoroge <prudence.n826@gmail.com>
2023-10-20 09:38:24 -0700

    Applied modifications to files: 0_simple_code.c hs hsh shell/2errors.c shell/2lists.c shell/builtin1.c shell/environment.c shell/errors.c shell/exits.c shell/getLine.c shell/getenv.c shell/getinfo.c shell/history.c shell/lists.c shell/main.c shell/memory.c shell/one.c shell/parser.c shell/realloc.c shell/shell.h shell/shell_loop.c shell/string.c shell/token.c

--- 0_simple_code.c
+++ 0_simple_code.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "shell.h"
 
 /**
 * main - Entry point
@@ -7,6 +8,36 @@
 
 int main(void)
 {
-	printf("Shell is the second group project\n");
+	char *buffer = NULL;
+	size_t bufsize = 0;
+	ssize_t chars;
+	char full_path[256];
+
+	while (1)
+	{
+		printf("$ ");
+		chars = getline(&buffer, &bufsize, stdin);
+
+		if (chars == -1)
+		{
+			if (isatty(STDIN_FILENO))
+				printf("\n");
+			free(buffer);
+			exit(EXIT_SUCCESS);
+		}
+
+		buffer[chars - 1] = '\0';
+
+		if (find_executable_in_path(buffer, full_path, sizeof(full_path)))
+		{
+			printf("Executable found at: %s\n", full_path);
+
+			handle_command(buffer);
+		}
+		else
+		{
+			fprintf(stderr, "%s: command not found\n", buffer);
+		}
+	}
 	return (0);
 }
